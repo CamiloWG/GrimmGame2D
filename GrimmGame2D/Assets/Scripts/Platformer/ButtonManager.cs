f92bef70e59d2fca70d5d10d111fe4d549ea222a
CamiloWG <40575328+CamiloWG@users.noreply.github.com>
2026-02-20 12:45:03 -0500

    Portada y nivel 1 creado

---
+++ GrimmGame2D/Assets/Scripts/Platformer/ButtonManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Metodo para boton de nuevo juego
+    public void StartGame()
+    {
+        Debug.Log("Iniciando el juego...");
+    }
+}
