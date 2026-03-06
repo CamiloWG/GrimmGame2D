91ec6c4af9dd98ef1b886bfbc7415933c4fec7d8
CamiloWG <40575328+CamiloWG@users.noreply.github.com>
2026-03-06 11:57:58 -0500

    Cambios

--- GrimmGame2D/Assets/Scripts/Platformer/PlayerMovement.cs
+++ GrimmGame2D/Assets/Scripts/Platformer/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private OldInput _oldInput;
-    public float speed;
+    public float speed = 1;
 
     // Start is called before the first frame update
     void Start()
