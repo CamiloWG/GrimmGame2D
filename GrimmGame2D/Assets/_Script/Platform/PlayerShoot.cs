833a7b5b9c70b2ddaf9dce2410c42e1aef2e1f37
CamiloWG <millos2423@gmail.com>
2026-04-08 20:40:24 -0500

    Cantidad maxima de balas 3 para el jugador

--- GrimmGame2D/Assets/_Script/Platform/PlayerShoot.cs
+++ GrimmGame2D/Assets/_Script/Platform/PlayerShoot.cs
@@ -7,19 +7,21 @@ public class PlayerShoot : MonoBehaviour
 {
     public GameObject bullet;
     public Transform shootPos;
+    public int amountBullets;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        amountBullets = 3;
     }
 
     public void Shoot(InputAction.CallbackContext context)
     {
-        if(context.started)
+        if(context.started && amountBullets > 0)
         {
             GameObject bulletPrefab = Instantiate(bullet, shootPos.position, shootPos.rotation);
             Destroy(bulletPrefab, 1);
+            amountBullets--;
         }
 
     }
