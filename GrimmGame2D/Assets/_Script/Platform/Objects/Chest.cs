1be01523d9a45cd0fe99f8eef0063c30d4d505f6
CamiloWG <millos2423@gmail.com>
2026-04-08 21:06:59 -0500

    Cofre de munición

---
+++ GrimmGame2D/Assets/_Script/Platform/Objects/Chest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            PlayerShoot playerBullets = collision.GetComponent<PlayerShoot>();
+            playerBullets.amountBullets += 10;
+            Destroy(gameObject, 1);
+        }
+    }
+}
